8648a6b18b89b0edc3b97abae6baf312922591c8
Jaqueline Kirino <bfatskih@gmail.com>
2021-10-15 19:27:36 -0300

    change phone number

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,15 +4,15 @@ import whats from '../whatsapp.png';
 const Home = () => {
   return <div className="App">
     <div className="board">
-      <h1>PROCURA SE</h1>
+      <h1>PROCURA-SE</h1>
       <h2>IZA</h2>
+      <p className="reward">RECOMPENSA R$500,00</p>
       <img src={iza} alt="Iza" />
       <p>Última vez vista em Pet shop Sumaré - Caraguatatuba - SP em 08/10/2021</p>
       <span>
         Por favor, entre em contato:
       </span>
-      <a href="https://api.whatsapp.com/send?phone=5511954798687"><img src={whats} alt="whatsapp" className="whats" /> (11) 95479-8687</a>
-      <a href="https://api.whatsapp.com/send?phone=5511973987530"><img src={whats} alt="whatsapp" className="whats" /> (11) 97398-7530</a>
+      <a href="https://api.whatsapp.com/send?phone=5512997121083"><img src={whats} alt="whatsapp" className="whats" /> (12) 99712-1083</a>
     </div>
   </div>
 }
